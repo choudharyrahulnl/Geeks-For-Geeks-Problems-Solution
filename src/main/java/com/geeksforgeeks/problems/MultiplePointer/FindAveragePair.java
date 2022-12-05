475e862960ab2631267ca0044c1f2476ac266ac9
TAACHRA0 <rahul.choudhary@swisscom.com>
2022-12-05 23:14:57 +0100

    initial commit

--- src/main/java/com/geeksforgeeks/problems/MultiplePointer/FindAveragePair.java
+++ src/main/java/com/geeksforgeeks/problems/MultiplePointer/FindAveragePair.java
@@ -3,19 +3,19 @@ package com.geeksforgeeks.problems.MultiplePointer;
 public class FindAveragePair {
 
     public static void main(String[] args) {
-        int[] nums1 = {2, 3, 4}; // 3.5
+        int[] nums1 = {2, 3, 4}; // 2.5
         int[] nums2 = {-2,0,1,2,4,5,6,7}; // 5.5
 
-        findAveragePair(nums1, 3.5);
-        findAveragePair(nums2, 5.5);
+        findAveragePair(nums1, 2.5f);
+        findAveragePair(nums2, 5.5f);
     }
 
-    private static void findAveragePair(int[] nums, double givenAvg) {
+    private static void findAveragePair(int[] nums, float givenAvg) {
         int start = 0;
         int end = nums.length-1;
 
         while (start < end) {
-            int avg = (nums[start] + nums[end]) / 2;
+            float avg = ((float) nums[start] + nums[end]) / 2;
 
             if(avg == givenAvg) {
                 System.out.println(start + ", " + end);
